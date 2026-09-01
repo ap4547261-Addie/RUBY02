ed8070982f0089a4470dd686c4bbe4fa240a7572
ap4547261-Addie <ap4547261@gmail.com>
2026-09-01 22:37:53 +0530

    Update Gemini API keys in config.py

--- config.py
+++ config.py
@@ -1,18 +1,18 @@
 # config.py
 import os
 
-os.environ["GEMINI_API_KEY1"] = "AIzaSy...YOUR_KEY_1"
-os.environ["GEMINI_API_KEY2"] = "AIzaSy...YOUR_KEY_2"
-os.environ["GEMINI_API_KEY3"] = "AIzaSy...YOUR_KEY_3"
-os.environ["GEMINI_API_KEY4"] = "AIzaSy...YOUR_KEY_4"
-os.environ["GEMINI_API_KEY5"] = "AIzaSy...YOUR_KEY_5"
-os.environ["GEMINI_API_KEY6"] = "AIzaSy...YOUR_KEY_6"
-os.environ["GEMINI_API_KEY7"] = "AIzaSy...YOUR_KEY_7"
-os.environ["GEMINI_API_KEY8"] = "AIzaSy...YOUR_KEY_8"
-os.environ["GEMINI_API_KEY9"] = "AIzaSy...YOUR_KEY_9"
+os.environ["GEMINI_API_KEY"] = "AQ.Ab8RN6KECpr2uiXehICjeq7F4fRhRnoskb0BnW--1XOSDAct-g"
+os.environ["GEMINI_API_KEY2"] = "AQ.Ab8RN6KSH7es07lTJeh20yg57S5RUC-1Sa04YCjvi_jHO7JvTg"
+os.environ["GEMINI_API_KEY3"] = "AQ.Ab8RN6IBChITS2mjtB7SU-A_vuWfpMWx3yLV4bY2tz-6hKK_7Q"
+os.environ["GEMINI_API_KEY4"] = "AQ.Ab8RN6LVEh3-76d8TdcsAr-SLBJUZhLk13zF00iU0CnyLTWhVA"
+os.environ["GEMINI_API_KEY5"] = "AQ.Ab8RN6L6fGBLIf9DnhDZqu83TPaFqfEA0Na_6Z96XbClxGYs_w"
+os.environ["GEMINI_API_KEY6"] = "AQ.Ab8RN6JEeA-FXbHjbrnIhuP8mQDTbQr1LUizDr8aXoyESlVXzg"
+os.environ["GEMINI_API_KEY7"] = "AQ.Ab8RN6Ki1iksci9Y320mmvKRvFnC9Vk_DIRM1y-oF2EdTWqSPQ"
+os.environ["GEMINI_API_KEY8"] = "AQ.Ab8RN6IRYbr39LXhBrSbdPkTLy9elHtgDYLQ6HdUXFETRKD_gw"
+os.environ["GEMINI_API_KEY9"] = "AQ.Ab8RN6Km1soDWCHqpOl6QFGfeQl0hphh1QFkObZaSoOrVk1GiA"
 
 # Fallback key
-os.environ["GEMINI_API_KEY"] = "AIzaSy...YOUR_FALLBACK_KEY"
+os.environ["GEMINI_API_KEY"] = "AQ.Ab8RN6Km1soDWCHqpOl6QFGfeQl0hphh1QFkObZaSoOrVk1GiA"
 
 PINECONE_API_KEY = None
 PINECONE_INDEX_HOST = None
